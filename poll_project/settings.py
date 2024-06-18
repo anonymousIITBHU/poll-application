f346dce3c29e934f358dc44a6e09b091af376450
anonymousIITBHU <78896308+anonymousIITBHU@users.noreply.github.com>
2024-06-18 17:31:29 +0530

    Update settings.py

--- poll_project/settings.py
+++ poll_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = ')*u*%4bhrf-l%6kwtzs@v9^z15*4rcro!kjtdf5t4vg_5-88!4'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*' , '.vercel.app']
 
 
 # Application definition
